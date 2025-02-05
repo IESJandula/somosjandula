d45f9b9c669cab06fb923e0e208dd22661037866
MiguelDevelop <miguelriu.dev@gmail.com>
2025-02-05 12:29:12 +0100

    Añadido Switch Recursos Previos y Finales

--- src/views/bookings/BookingsAdminPage.vue
+++ src/views/bookings/BookingsAdminPage.vue
@@ -38,8 +38,17 @@
         </ion-col>
       </ion-row>
       <div class="title-container">
-      <h1 class="title">Crear Recurso</h1>
+        <h1 class="title">Crear Recurso</h1>
+        <div class="switch-container">
+            <span>Previos</span>
+            <label class="switch">
+                <input type="checkbox">
+                <span class="slider"></span>
+            </label>
+            <span>Finales</span>
+        </div>
     </div>
+      
       <ion-row>
         <ion-col size="12">
           <ion-item>
@@ -416,6 +425,68 @@ tr:hover td {
 .modal-content button:hover {
   background-color: #0056b3;
 }
+
+.title-container {
+            display: flex;
+            align-items: center;
+            position: relative;
+            width: 100%;
+            padding: 20px;
+        }
+        .title {
+            margin: 0;
+            font-size: 24px;
+        }
+        .switch-container {
+            position: absolute;
+            left: 50%;
+            transform: translateX(-50%);
+            display: flex;
+            align-items: center;
+            gap: 10px;
+        }
+        .switch-container span {
+            font-size: 24px;
+        }
+        .switch {
+            position: relative;
+            display: inline-block;
+            width: 60px;
+            height: 34px;
+        }
+        .switch input {
+            opacity: 0;
+            width: 0;
+            height: 0;
+        }
+        .slider {
+            position: absolute;
+            cursor: pointer;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background-color: #ccc;
+            transition: .4s;
+            border-radius: 34px;
+        }
+        .slider:before {
+            position: absolute;
+            content: "";
+            height: 26px;
+            width: 26px;
+            left: 4px;
+            bottom: 4px;
+            background-color: white;
+            transition: .4s;
+            border-radius: 50%;
+        }
+        input:checked + .slider {
+            background-color: #2196F3;
+        }
+        input:checked + .slider:before {
+            transform: translateX(26px);
+        }
 
 @media (max-width: 768px) {
   table {
