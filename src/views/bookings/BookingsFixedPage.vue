e0614cd8d8da2223456ad8d3e6a90d16ece5130c
pakkk <paco.benitez.chico@gmail.com>
2025-03-15 10:12:15 +0100

    tilde en la palabra "más"

--- src/views/bookings/BookingsFixedPage.vue
+++ src/views/bookings/BookingsFixedPage.vue
@@ -411,7 +411,7 @@ watch(recursoSeleccionado, () => {
 
   if (recursoSeleccionadoCompartible.value) {
     mensajeInformativo = 'Recuerda, este recurso SÍ se puede compartir en el mismo tramo horario'
-    mensajeIncidencia = '¿Encontraste algun problema en el aula o necesitas mas recursos? Crea una incidencia '
+    mensajeIncidencia = '¿Encontraste algun problema en el aula o necesitas más recursos? Crea una incidencia '
   }
   else {
     mensajeInformativo = ''
