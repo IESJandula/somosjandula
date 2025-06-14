2f6f0d3b93d749818eed331a308ae2e7d857239d
azamlop <alexzambranalopez2005@gmail.com>
2025-06-14 13:22:39 +0200

    LoginPage refactorizado sin ionic

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,360 +1,302 @@
 <template>
-  <ion-page>
-    <ion-content class="ion-padding" fullscreen>
-      <div class="form-container">
-        <!-- Añadimos el texto "SOMOS" con la clase animada por encima del título -->
-        <div class="somos-container">
-          <span class="somos-text">SOMOS</span>
-        </div>
-        <h1 class="title">J&aacute;ndula</h1>
-        
-        <!-- Contenedor para las frases transitorias -->
-        <div class="features-container">
-          <div class="feature-text impresion-remota">Impresión remota</div>
-          <div class="feature-text reserva-recursos">Reserva de recursos</div>
-          <div class="feature-text mat-agrup">Matriculaciones y agrupamientos</div>
-          <div class="feature-text gestion-ap">Gestión de alumnado y profesorado</div>
-          <div class="feature-text asignacion-materias">Asignación de materias y reducciones</div>
-          <div class="feature-text reserva-proyectores">Proximamente ... uso de proyectores</div>
-        </div>
-        
-        <div class="buttons-container">
-          <div class="google-login-button" @click="loginWithGoogle">
-            <ion-icon :icon="googleIcon" class="google-icon"></ion-icon>
-            <span>Conecta con Google</span>
-          </div>
-          
-          <!-- Botón permanente del coche Lince -->
-          <div class="lince-button" @click="verCocheLince">
-            <ion-icon :icon="carIcon" class="lince-icon"></ion-icon>
-            <span>¡Vive en directo nuestro Eco-Jándula!</span>
-          </div>
-        </div>
+  <div class="cnt__login">
+    <div class="login__content">
+
+      <div>
+        <h1 class="login__title">
+          <div class="">Somos</div>
+          <div class="">Jándula</div>
+        </h1>
       </div>
 
-      <ion-toast :is-open="isToastOpen" :message="toastMessage" :color="toastColor" duration="2500"
-          @did-dismiss="() => isToastOpen = false" position="top"></ion-toast>
-      </ion-content>
-  </ion-page>
+      <div class="login__extra-info">
+        <transition name="fade">
+          <span v-if="fade" :key="dynamicInfo">{{ dynamicInfo }}</span>
+        </transition>
+      </div>
+
+      <div class="login__action">
+        <button class="login__button login__button--default" @click="loginWithGoogle">
+          Conecta con Google
+        </button>
+
+        <button class="login__button login__button--lince" @click="verCocheLince">
+          ¡Descubre nuestro Eco-Jándula!
+        </button>
+      </div>
+
+    </div>
+
+    <div class="login__photo">
+      <img src="../assets/img/login/login.jpg" alt="login image">
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
-import { IonContent, IonIcon, IonToast, IonPage } from '@ionic/vue';
-import { logoGoogle as googleIcon, car as carIcon } from 'ionicons/icons';
-import { signInWithPopup, signOut } from "firebase/auth";
-import { crearToast } from '@/utils/toast.js';
-import { obtenerRolesUsuario } from '@/services/firebaseService.js';
+import { signInWithPopup, getAuth, GoogleAuthProvider } from "firebase/auth";
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { firebaseConfig } from '@/environment/firebaseConfig';
-import { setLoggingInStatus } from '@/main'; // Importa la función del estado
+import { obtenerRolesUsuario } from '@/services/firebaseService.js';
+import { crearToast } from '@/utils/toast.js';
+import { setLoggingInStatus } from '@/main';
 
-// Inicializa Firebase
-const app = initializeApp(firebaseConfig) ;
-const auth = getAuth(app) ;
-
-const googleProvider = new GoogleAuthProvider() ;
-
-// Inicializar el servicio de mensajería
-// const messaging = getMessaging(app) ;
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
 const router = useRouter();
 
-// Variables para el toast
-const isToastOpen = ref(false);
-const toastMessage = ref('');
-const toastColor = ref('success');
+const mensajes = [
+  "Impresión remota",
+  "Reserva de recursos",
+  "Matriculaciones y agrupamientos",
+  "Gestión de alumnado y profesorado",
+  "Asignación de materias y reducciones",
+  "Próximamente... uso de proyectos"
+];
 
-const loginWithGoogle = async () =>
-{
-  try
-  {
-    // Indicamos que el usuario está iniciando sesión
+const dynamicInfo = ref(mensajes[0]);
+const currentIndex = ref(0);
+const fade = ref(true);
+
+onMounted(() => {
+  setInterval(() => {
+    fade.value = false;
+    setTimeout(() => {
+      currentIndex.value = (currentIndex.value + 1) % mensajes.length;
+      dynamicInfo.value = mensajes[currentIndex.value];
+      fade.value = true;
+    }, 300);
+  }, 3000);
+});
+
+const loginWithGoogle = async () => {
+  try {
     setLoggingInStatus(true);
-
-    // Login a través de Google
-    const result    = await signInWithPopup(auth, googleProvider);
-
-    // Obtenemos el token JWT
-    const user      = result.user ;
-
-    // Validamos el usuario en el sistema
-    const userRoles = await obtenerRolesUsuario(toastMessage, toastColor, isToastOpen) ;
-
-    // Verificamos si hay una redirección pendiente (guardada en el router)
-    const redirectPath = router.currentRoute.value.query.redirect || '/printers/print'; // 'Dashboard' es la ruta por defecto
+    const result = await signInWithPopup(auth, googleProvider);
+    const user = result.user;
+    const userRoles = await obtenerRolesUsuario();
+    const redirectPath = router.currentRoute.value.query.redirect || '/printers/print';
     router.push({ path: redirectPath });
-  }
-  catch (error)
-  {
-    // Crear toast
-    crearToast(toastMessage, toastColor, isToastOpen, "danger", error.message) ;
-  }
-  finally
-  {
-    // Restablecemos el estado después del login
-    setLoggingInStatus(false); 
+  } catch (error) {
+    crearToast("Error", "danger", true, error.message);
+  } finally {
+    setLoggingInStatus(false);
   }
 };
 
-// Función para el botón del coche Lince
 const verCocheLince = () => {
-  // Aquí puedes añadir la lógica para ver el coche Lince en directo
-  window.open('https://telemetrialince.iesjandula.es/', '_blank'); // Ajusta esta URL según corresponda
+  window.open('https://telemetrialince.iesjandula.es/', '_blank');
 };
-
 </script>
 
 <style scoped>
-/* Estilos generales */
-body {
-  background-color: var(--background-light);
-  font-family: 'Roboto', sans-serif;
-}
-
-.ion-padding {
-  --background: var(--ion-padding-bg-light);
-}
-
-/* Modo claro */
-.form-container {
-  width: 100%;
-  max-width: 350px;
-  background-color: var(--form-bg-light);
-  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-  border-radius: 10px;
+body * {
   box-sizing: border-box;
-  padding: 20px 30px;
-  margin: auto;
-  margin-top: 10%;
-  font-family: 'Roboto', sans-serif;
-  position: relative;
 }
 
-.title {
-  text-align: center;
-  margin: 10px 0 30px 0;
-  font-size: 28px;
-  font-weight: 800;
-  color: var(--text-color-light);
-  z-index: 1;
-}
-
-.somos-container {
-  position: absolute;
-  top: -0.25em;
-  width: 100%;
-  height: 40px;
+body {
+  height: 100dvh;
+  margin: 0;
+  padding: 0;
+  font-family: "Roboto", sans-serif;
   overflow: hidden;
-  z-index: 2;
 }
 
-.somos-text {
-  position: absolute;
-  left: -100%;
-  top: 0;
-  font-size: 24px;
-  font-weight: bold;
-  color: var(--text-color-light);
-  animation: moveToCenter 10s ease-in-out forwards;
-  white-space: nowrap;
+ul, ol {
+  list-style: none;
+  padding: 0;
 }
 
-@keyframes moveToCenter {
-  0% {
-    left: -100%;
-    opacity: 1;
-  }
-  20% {
-    left: 50%;
-    transform: translateX(-85%);
-    opacity: 1;
-  }
-  80% {
-    left: 50%;
-    transform: translateX(-85%);
-    opacity: 1;
-  }
-  100% {
-    left: 50%;
-    transform: translateX(-85%);
-    opacity: 0;
-  }
-}
-
-.buttons-container {
+img {
   width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.cnt__login {
+  display: flex;
+  color: white;
+  background-color: #0B2E47;
+  min-height: 100dvh;
+}
+
+.login__content {
   display: flex;
   flex-direction: column;
-  justify-content: flex-start;
-  margin-top: 20px;
-  gap: 15px;
-}
-
-.google-login-button {
-  border-radius: 20px;
-  box-sizing: border-box;
-  padding: 10px 15px;
-  background-color: var(--button-bg-light);
-  box-shadow: rgba(0, 0, 0, 0.16) 0px 10px 36px 0px,
-    rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;
-  cursor: pointer;
-  display: flex;
-  justify-content: center;
   align-items: center;
-  font-size: 11px;
-  gap: 5px;
+  justify-content: center;
+  width: 50%;
 }
 
-.google-login-button {
-  border: 2px solid var(--button-border-light);
-}
-
-.google-icon {
-  font-size: 18px;
-  margin-bottom: 1px;
-  color: var(--google-icon-light);
-}
-
-/* Características con transiciones */
-.features-container {
-  position: relative;
-  height: 40px;
+.login__content > * {
   width: 100%;
-  text-align: center;
-  margin: 0 0 20px 0;
-  overflow: hidden;
+  margin-bottom: 70px;
+  padding: 0 76px 0;
 }
 
-.feature-text {
-  position: absolute;
-  width: 100%;
-  font-size: 16px;
-  font-weight: 600;
-  color: var(--text-color-light);
+.login__content > :last-child {
+  margin-bottom: 0;
+}
+
+.login__title {
+  display: flex;
+  font-size: 54px;
+}
+
+.login__title--w2 {
+  text-transform: uppercase;
+}
+
+h1 {
+  margin: 0;
+}
+
+.login__extra-info {
+  font-size: 22px;
+  min-height: 30px;
+}
+
+.login__action {
+  display: flex;
+  gap: 25px;
+}
+
+.login__button {
+  padding: 15px 30px;
+  border-radius: 12px;
+  border: solid 1px black;
+  color: white;
+  font-size: 22px;
+}
+
+.login__button:hover {
+  cursor: pointer;
+  transition: 0.4s;
+}
+
+.login__button--default {
+  background-color: #269AED;
+}
+
+.login__button--default:hover {
+  background-color: #1a4769;
+  border: solid 1px rgba(255, 255, 255, 0.21);
+}
+
+.login__button--lince {
+  background-color: transparent;
+  border: solid 1px rgba(255, 255, 255, 0.21);
+}
+
+.login__button--lince:hover {
+  background-color: #67aee5;
+}
+
+.login__photo {
+  width: 50%;
+}
+
+/* Animación de fade */
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+.fade-enter-from, .fade-leave-to {
   opacity: 0;
 }
 
-.impresion-remota {
-  animation: fadeInOut 18s ease-in-out 0s infinite;
-}
-
-.reserva-recursos {
-  animation: fadeInOut 18s ease-in-out 3s infinite;
-}
-
-.mat-agrup {
-  animation: fadeInOut 18s ease-in-out 6s infinite;
-}
-
-.gestion-ap {
-  animation: fadeInOut 18s ease-in-out 9s infinite;
-}
-
-.asignacion-materias {
-  animation: fadeInOut 18s ease-in-out 12s infinite;
-}
-
-.reserva-proyectores {
-  animation: fadeInOut 18s ease-in-out 15s infinite;
-}
-
-@keyframes fadeInOut {
-  0% {
-    opacity: 0;
-    transform: translateY(20px);
+/* Responsivo */
+@media (max-width: 1200px) {
+  .login__title {
+    font-size: 38px;
   }
-  2.78% { /* Start showing (fade in ends ~0.5s) */
-    opacity: 1;
-    transform: translateY(0);
+
+  .login__extra-info {
+    font-size: 18px;
   }
-  13.89% { /* Stay visible until (~2.5s) */
-    opacity: 1;
-    transform: translateY(0);
-  }
-  16.67% { /* Start fading out (~3s) */
-    opacity: 0;
-    transform: translateY(-20px);
-  }
-  100% {
-    opacity: 0;
-    transform: translateY(-20px);
+
+  .login__button {
+    font-size: 18px;
   }
 }
 
-/* Botón del coche Lince */
-.lince-button {
-  border-radius: 20px;
-  box-sizing: border-box;
-  padding: 10px 15px;
-  background-color: var(--button-bg-light);
-  box-shadow: rgba(0, 0, 0, 0.16) 0px 10px 36px 0px,
-    rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;
-  cursor: pointer;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 11px;
-  gap: 5px;
-  border: 2px solid var(--button-border-light);
+@media (max-width: 1024px) {
+  .login__title {
+    font-size: 34px;
+  }
+
+  .login__extra-info {
+    font-size: 16px;
+  }
+
+  .login__button {
+    padding: 10px 25px;
+    font-size: 16px;
+  }
 }
 
-.lince-icon {
-  font-size: 18px;
-  margin-bottom: 1px;
-  color: var(--google-icon-light);
+@media (max-width: 768px) {
+  .cnt__login {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+
+  .login__content {
+    width: 100%;
+    margin: 40px 0;
+  }
+
+  .login__photo {
+    width: 100%;
+  }
+
+  .login__title {
+    font-size: 34px;
+  }
+
+  .login__extra-info {
+    font-size: 16px;
+  }
+
+  .login__button {
+    font-size: 16px;
+  }
 }
 
-/* Modo oscuro */
-@media (prefers-color-scheme: dark) {
-  body {
-    background-color: var(--background-dark);
+@media (max-width: 452px) {
+  .login__content {
+    margin: 80px 0;
   }
 
-  .ion-padding {
-    --background: var(--ion-padding-bg-dark);
+  .login__title {
+    font-size: 28px;
   }
 
-  .form-container {
-    background-color: var(--form-bg-dark);
-    box-shadow: rgba(255, 255, 255, 0.1) 0px 5px 15px;
-    border: 1px solid #444;
+  .login__extra-info {
+    font-size: 12px;
   }
 
-  .title {
-    color: var(--text-color-dark);
+  .login__button {
+    font-size: 12px;
+  }
+}
+
+@media (max-width: 360px) {
+  .login__title {
+    font-size: 22px;
   }
 
-  .somos-text {
-    color: var(--text-color-dark);
+  .login__extra-info {
+    font-size: 10px;
   }
 
-  .google-login-button {
-    background-color: var(--button-bg-dark);
-    border: 2px solid var(--button-border-dark);
-  }
-
-  .google-icon {
-    color: var(--google-icon-dark);
-  }
-
-  .feature-text {
-    color: var(--text-color-dark);
-  }
-  
-  .lince-button {
-    background-color: var(--button-bg-dark);
-    border: 2px solid var(--button-border-dark);
-  }
-  
-  .lince-button:hover {
-    background-color: rgba(255, 255, 255, 0.1);
-  }
-
-  .lince-icon {
-    color: var(--google-icon-dark);
+  .login__button {
+    padding: 8px 15px;
+    font-size: 10px;
   }
 }
 </style>
