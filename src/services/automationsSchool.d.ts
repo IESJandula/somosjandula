e53df4f63f9949de5c1f463d3b969766d52442e2
adriLunaDelMoral <adrian.luna.del.moral@gmail.com>
2025-12-13 20:26:29 +0100

    DOM_004: crear carpeta en views "automationsSchool". Dentro de ella, una vista de administración. Crear en routing un path hacia la vista anterior. Crear el fichero d.ts y .js con las llamadas al servidor

--- src/services/automationsSchool.d.ts
+++ src/services/automationsSchool.d.ts
@@ -1,30 +1,18 @@
-export declare function postSensorBooleano(toastMessage: Ref<string>,
+export declare function crearSensorBooleano(toastMessage: Ref<string>,
                                       toastColor: Ref<string>,
                                       isToastOpen: Ref<boolean>): Promise<void>;
 
-export declare function getpostSensorBooleano(toastMessage: Ref<string>,
+export declare function obtenerSensorBooleano(toastMessage: Ref<string>,
                                        toastColor: Ref<string>,
                                        isToastOpen: Ref<boolean>): Promise<string[]>;
 
-export declare function deletepostSensorBooleano(toastMessage: Ref<string>,
-                                        toastColor: Ref<string>,
-                                        isToastOpen: Ref<boolean>): Promise<string[]>;
-
-export declare function postSensorNumerico(toastMessage: Ref<string>,
+export declare function crearSensorNumerico(toastMessage: Ref<string>,
                                     toastColor: Ref<string>,
                                     isToastOpen: Ref<boolean>): Promise<string[]>;
 
-export declare function getSensorNumerico(toastMessage: Ref<string>,
+export declare function obtenerSensorNumerico(toastMessage: Ref<string>,
                                     toastColor: Ref<string>,
                                     isToastOpen: Ref<boolean>): Promise<string[]>;
-
-export declare function deleteSensorNumerico(toastMessage: Ref<string>,
-                                    toastColor: Ref<string>,
-                                    isToastOpen: Ref<boolean>): Promise<string[]>;
-
-export declare function obtenerSensores(toastMessage: Ref<string>,
-                                    toastColor: Ref<string>,
-                                    isToastOpen: Ref<boolean>): Promise<[]>;
 
 export declare function obtenerActuadores(toastMessage: Ref<string>,
                                     toastColor: Ref<string>,
