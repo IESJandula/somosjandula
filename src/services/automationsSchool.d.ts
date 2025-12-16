f2db9e93fe5117e00dede76c19e796c5372031c4
ManuCarSeg24 <manucarseg@gmail.com>
2025-12-16 13:27:19 +0100

    DOM_004: cabeceras tienen que ser las mismas en automationSchool.d.ts que en automationSchool.js

--- src/services/automationsSchool.d.ts
+++ src/services/automationsSchool.d.ts
@@ -1,23 +1,31 @@
 export declare function crearSensorBooleano(toastMessage: Ref<string>,
-                                      toastColor: Ref<string>,
-                                      isToastOpen: Ref<boolean>): Promise<void>;
+                                        toastColor: Ref<string>,
+                                        isToastOpen: Ref<boolean>,
+                                        mac: string,
+                                        estado: boolean,
+                                        nombreUbicacion: string): Promise<void>;
 
 export declare function obtenerSensorBooleano(toastMessage: Ref<string>,
-                                       toastColor: Ref<string>,
-                                       isToastOpen: Ref<boolean>): Promise<string[]>;
+                                        toastColor: Ref<string>,
+                                        isToastOpen: Ref<boolean>): Promise<string[]>;
 
 export declare function crearSensorNumerico(toastMessage: Ref<string>,
-                                    toastColor: Ref<string>,
-                                    isToastOpen: Ref<boolean>): Promise<string[]>;
+                                        toastColor: Ref<string>,
+                                        isToastOpen: Ref<boolean>,
+                                        mac: string,
+                                        estado: number,
+                                        nombreUbicacion: string,
+                                        umbralMinimo: number,
+                                        umbralMaximo: number): Promise<string[]>;
 
 export declare function obtenerSensorNumerico(toastMessage: Ref<string>,
-                                    toastColor: Ref<string>,
-                                    isToastOpen: Ref<boolean>): Promise<string[]>;
+                                        toastColor: Ref<string>,
+                                        isToastOpen: Ref<boolean>): Promise<string[]>;
 
 export declare function obtenerActuadores(toastMessage: Ref<string>,
-                                    toastColor: Ref<string>,
-                                    isToastOpen: Ref<boolean>): Promise<[]>;
+                                        toastColor: Ref<string>,
+                                        isToastOpen: Ref<boolean>): Promise<[]>;
                                     
 export declare function obtenerUbicaciones(toastMessage: Ref<string>,
-                                    toastColor: Ref<string>,
-                                    isToastOpen: Ref<boolean>): Promise<[]>;
+                                        toastColor: Ref<string>,
+                                        isToastOpen: Ref<boolean>): Promise<[]>;
