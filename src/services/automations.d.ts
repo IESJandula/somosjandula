1f26d4c14cfd07047d53c21cebbb6e8fe8af313a
Paco Benitez Chico <paco.benitez.chico@gmail.com>
2026-04-19 17:18:19 +0200

    quitar cambios de la tarea DOM_261 ya que lo que tenía simplemente era código de pruebas

--- src/services/automations.d.ts
+++ src/services/automations.d.ts
@@ -62,13 +62,6 @@ export type ActuadorResponseDto =
   tipo: string
   numeroReles?: number | null
 }
-export type ActuadorAccionPendienteResponseDto =
-{
-  accionId: number
-  orden: string
-  keyword?: string
-  indiceRele?: number | null
-}
 
 export declare function obtenerDispositivos(toastMessage: Ref<string>,
                                             toastColor: Ref<string>,
@@ -252,8 +245,3 @@ export declare function obtenerSensorNumericoPaginacion(toastMessage: Ref<string
                                                         isToastOpen: Ref<boolean>,
                                                         page?: number,
                                                         size?: number): Promise<PageResponseDto<any>>;
-                                                        
-export declare function obtenerAccionesPendientesActuador(toastMessage: Ref<string>,
-                                                          toastColor: Ref<string>,
-                                                          isToastOpen: Ref<boolean>,
-                                                          mac: Ref<string> | string): Promise<ActuadorAccionPendienteResponseDto[]>
